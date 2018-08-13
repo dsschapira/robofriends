00d2b304e9b5924d373073574438ac0271633d65
Dan Schapira <dsschapira@gmail.com>
2018-08-13 10:34:40 -0400

    make better column display

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
   return (
-    <div style={{ overflowY: 'scroll', border: '5px solid transparent', maxHeight: '700px', paddingLeft: '1em', paddingRight: '1em'}}>
+    <div style={{ overflowY: 'scroll', border: '5px solid transparent', maxHeight: '700px', paddingLeft: '1em', paddingRight: '1em', display:'flex'}}>
       {props.children}
     </div>
   );
